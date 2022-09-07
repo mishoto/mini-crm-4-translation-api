9a8183acb72d8a68fe4c463639f7aa2ab15b27b9
mishoto <mihail.dev.io@gmail.com>
2022-09-07 16:42:52 +0300

    change URL to api-v1.0/auth

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes');
-const usersRouter = require('./routes/users');
+const authRouter = require('./routes/users/auth');
 
 const app = express();
 
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api-v1.0/auth', authRouter);
 
 module.exports = app;
